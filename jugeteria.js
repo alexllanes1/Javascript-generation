4f958e8e090a6f11315fa4b50cb8d8f2128d186a
alexllanes1 <alexllanes026@gmail.com>
2023-05-23 21:52:18 -0600

    Se añadieron prompts y funciones

--- jugeteria.js
+++ jugeteria.js
@@ -2,19 +2,32 @@
 
 console.log("Ejercicio 2: Jugueteria");
 
+//Prompts
+var payasos=prompt("Ingresa la cantidad de payasos");
+var muñecas=prompt("Ingresa la cantidad de muñecas");
 //número de items
-payasos = 27;
-muñecas = 14;
+//payasos = 27;
+//muñecas = 14;
 
 //peso de items
-pesoPayaso = 112;
-pesoMuñeca = 75;
+const pesoPayaso = 112;
+const pesoMuñeca = 75;
 
-//operacines
+//operaciones
 pesoPayasoTotal = payasos*pesoPayaso;
 pesoMuñecaTotal = muñecas*pesoMuñeca;
 
+/*
+Calculo del peso total
 pesoTotal = pesoPayasoTotal+pesoMuñecaTotal;
+*/
+
+//funcion del peso total
+function pesoTotal (pesoPayasoTotal,pesoMuñecaTotal){
+    operacion = pesoPayasoTotal+pesoMuñecaTotal;
+    return operacion;
+}
 
 //print
-console.log("El peso total del paquete es", pesoTotal, "gramos");
+const resultado = pesoTotal(pesoPayasoTotal, pesoMuñecaTotal);
+console.log("El peso total del paquete es " + resultado + " gramos");
